ede3e5074eb23da1525ae98f6efe305a269e0e91
Sam Bostock <bostocksamuel@gmail.com>
2017-04-17 01:48:04 -0400

    Allow todos to be completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417045734) do
+ActiveRecord::Schema.define(version: 20170417054512) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.datetime "completed_at"
   end
 
 end
